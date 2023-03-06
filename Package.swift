797782845a5edb2d2eed78fa0799306659fa781a
Viet Dzung <dzungpv@users.noreply.github.com>
2023-03-06 18:06:41 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "KWDrawerController",
-            dependencies: ["RxSwift", "RxCocoa"],
+            dependencies: ["RxSwift"],
             path: "DrawerController"),
     ]
 )
