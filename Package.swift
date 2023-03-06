902ed2787408f86c50baca25fc2e43969d0c6eda
Viet Dzung <dzungpv@users.noreply.github.com>
2023-03-06 18:27:34 +0700

    Fix SPM

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "KWDrawerController",
-            dependencies: ["RxSwift"],
+            dependencies: ["RxSwift",
+                           .product(name: "RxCocoa", package: "RxSwift")]),
             path: "DrawerController"),
     ]
 )
