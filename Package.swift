0b08e05c14723ae5eb41b2c2fa48939f5dba672e
Viet Dzung <dzungpv@users.noreply.github.com>
2023-03-07 08:18:49 +0700

    Downgrade RxSwift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.5.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("4.3.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
